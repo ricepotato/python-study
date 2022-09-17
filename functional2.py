ba06ca3bf85a55f692ef71078a8b5e4cf9568f3b
ricepotato <sukjun40@naver.com>
2022-09-17 09:13:40 +0900

    closure

--- functional2.py
+++ functional2.py
@@ -7,9 +7,12 @@ from exchange import Exchange
 TradingStrategyFunction = Callable[[list[int]], bool]
 
 
-def should_buy_avg(prices: list[int]) -> bool:
-    list_window = prices[-3:]
-    return prices[-1] < statistics.mean(list_window)
+def should_by_avg_closure(windows_size: int) -> TradingStrategyFunction:
+    def should_buy_avg(prices: list[int]) -> bool:
+        list_window = prices[-windows_size:]
+        return prices[-1] < statistics.mean(list_window)
+
+    return should_buy_avg
 
 
 def should_sell_avg(prices: list[int]) -> bool:
@@ -21,8 +24,11 @@ def should_buy_minmax(prices: list[int]) -> bool:
     return prices[-1] < 32_000_00
 
 
-def should_sell_minmax(prices: list[int]) -> bool:
-    return prices[-1] > 33_000_00
+def should_sell_minmax_closure(max_price: int) -> TradingStrategyFunction:
+    def should_sell_minmax(prices: list[int]) -> bool:
+        return prices[-1] > max_price
+
+    return should_sell_minmax
 
 
 @dataclass
@@ -50,7 +56,9 @@ def main() -> None:
     exchange.connect()
 
     bot = TradingBot(
-        exchange=exchange, buy_strategy=should_buy_avg, sell_strategy=should_sell_avg
+        exchange=exchange,
+        buy_strategy=should_by_avg_closure(3),
+        sell_strategy=should_sell_avg,
     )
     bot.run("BTC/USD")
 
